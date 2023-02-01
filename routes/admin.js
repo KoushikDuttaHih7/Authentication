df6896909fd56003fd0b8bb92882613b74b07600
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-02-01 13:12:59 +0530

    259.  Using Middleware to Protect Routes

--- routes/admin.js
+++ routes/admin.js
@@ -3,28 +3,29 @@ const path = require("path");
 const express = require("express");
 
 const adminController = require("../controllers/admin");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
 // /admin/add-product => GET
 // To add products
-router.get("/add-product", adminController.getAddProduct);
+router.get("/add-product", isAuth, adminController.getAddProduct);
 
 // /admin/product => GET
 // To show products in the Admin products page
-router.get("/products", adminController.getProducts);
+router.get("/products", isAuth, adminController.getProducts);
 
 // /admin/add-product => POST
 // To show products on main page after adding products
-router.post("/add-product", adminController.postAddProduct);
+router.post("/add-product", isAuth, adminController.postAddProduct);
 
 // To edit products
-router.get("/edit-product/:productId", adminController.getEditProduct);
+router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
 
 // TO update..edited product
-router.post("/edit-product", adminController.postEditProduct);
+router.post("/edit-product", isAuth, adminController.postEditProduct);
 
 // To delete product
-router.post("/delete-product", adminController.postDeleteProduct);
+router.post("/delete-product", isAuth, adminController.postDeleteProduct);
 
 exports.routes = router;
--- routes/shop.js
+++ routes/shop.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 
 const shopController = require("../controllers/shop");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
@@ -16,18 +17,18 @@ router.get("/products", shopController.getProducts);
 router.get("/products/:productId", shopController.getProduct);
 
 // This is for my cart view
-router.get("/cart", shopController.getCart);
+router.get("/cart", isAuth, shopController.getCart);
 
 // This is to add to my cart
-router.post("/cart", shopController.postCart);
+router.post("/cart", isAuth, shopController.postCart);
 
 // this is for deleting products from the cart
-router.post("/cart-delete-item", shopController.postCartDeleteProduct);
+router.post("/cart-delete-item", isAuth, shopController.postCartDeleteProduct);
 
 // This is for my order view
-router.post("/create-order", shopController.postOrder);
+router.post("/create-order", isAuth, shopController.postOrder);
 
 // This is for checkout view
-router.get("/orders", shopController.getOrders);
+router.get("/orders", isAuth, shopController.getOrders);
 
 module.exports = router;
