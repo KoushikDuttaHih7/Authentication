9a32698c158c28885f13a6d29f8069373979f0b0
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-02-01 11:59:49 +0530

    255. Encrypting Passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
-const user = require("../models/user");
+const bcrypt = require("bcrypt");
+
 const User = require("../models/user");
 
 // This is for Login view
@@ -44,9 +45,12 @@ exports.postSignup = (req, res, next) => {
       if (userDoc) {
         return res.redirect("/signup");
       }
+      return bcrypt.hash(password, 12);
+    })
+    .then((hashedPassword) => {
       const user = new User({
         email: email,
-        password: password,
+        password: hashedPassword,
         cart: { items: [] },
       });
       return user.save();
