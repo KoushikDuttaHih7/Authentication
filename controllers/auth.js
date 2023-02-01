23058e8880ba6fc1da9a302c36a4df05350706fe
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-02-01 13:00:51 +0530

    257. Adding the Signin(login) Functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 
 const User = require("../models/user");
 
@@ -23,14 +23,30 @@ exports.getSignup = (req, res, next) => {
 
 // This is for Login view
 exports.postLogin = (req, res, next) => {
-  User.findById("63d9f10468651c6b61dbf7a3")
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
     .then((user) => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        console.log(err);
-        res.redirect("/");
-      });
+      if (!user) {
+        return res.redirect("/login");
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save((err) => {
+              console.log(err);
+              res.redirect("/");
+            });
+          }
+          res.redirect("/login");
+        })
+        .catch((err) => {
+          console.log(err);
+          res.redirect("/login");
+        });
     })
     .catch((err) => console.log(err));
 };
@@ -55,7 +71,7 @@ exports.postSignup = (req, res, next) => {
           });
           return user.save();
         })
-        .then((results) => {
+        .then((result) => {
           res.redirect("/login");
         });
     })
